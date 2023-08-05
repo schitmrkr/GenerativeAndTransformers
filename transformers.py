3ca90375dae9e50385e89441b4d1c21d76bb8601
schitmrkr <schitmrkr@gmail.com>
2023-08-05 20:09:04 +0545

    transformer

---
+++ transformers.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+class SelfAttention(nn.Module):
+    def __init__(self, embed_size, heads):
+        super(SelfAttention, self).__init__()
+        self.embed_size = embed_size
+        self.heads = heads
+        self.head_dim = embed_size // heads
+
+        assert (self.head_dim * heads == embed_size), "Embed size needs to be divisible by head"
+
+        self.values = nn.Linear(self.head_dim, self.head_dim, bias=False)  #self.head_dim can be vocab size
+        self.keys = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.queries = nn.Linear(self.head_dim, self.head_dim, bias=False)
+
+        self.fc_out = nn.Linear(heads * self.head_dim, embed_size)
+
+    def forward(self, values, keys, query, mask):
+        pass
+        
